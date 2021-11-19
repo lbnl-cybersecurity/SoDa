e07987e2ed05e036b72cf6d668ce43e60eccf2f4
toanngo <toanngosy@gmail.com>
2021-11-19 10:38:19 -0800

    add config

---
+++ soda/config.py
@@ -0,0 +1,6 @@
+API_KEY = '' # NSRDB api key
+YOUR_NAME = '' # Your full name, use '+' instead of spaces.
+REASON_FOR_USE = '' # Your reason for using the NSRDB.
+YOUR_AFFILIATION = '' # Your affiliation
+YOUR_EMAIL = '' # Your email address
+MAILING_LIST = 'false'
